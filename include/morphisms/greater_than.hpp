59862f795e541ecf5a1255a03c6b246b3c0cb1d9
alclol <lyuan1227@gmail.com>
2020-04-12 02:19:24 -0700

    comparator struct

--- include/morphisms/greater_than.hpp
+++ include/morphisms/greater_than.hpp
@@ -8,18 +8,38 @@
 #define MANGO_GREATER_THAN_H
 
 template <class T>
-struct GreaterThanStruct {
+struct CompStruct {
 private:
-   T threshold;
+    T threshold;
 
 public:
-   GreaterThanStruct(T _threshold) : threshold(_threshold) {};
+    CompStruct(T _threshold) : threshold(_threshold) {};
 
-   template <class Func>
-   typename std::enable_if_t<std::is_same<Func, Max>::value, Lattice<bool, Or>>
-   operator()(Lattice<T, Func> target) {
-      return Lattice(target.reveal()>this->threshold, Or{});
-   }
+    template <class Func>
+    typename std::enable_if_t<std::is_same<Func, Max>::value, Lattice<bool, Or>>
+    greater_than(Lattice<T, Func> target) {
+       return Lattice(target.reveal()>this->threshold, Or{});
+    }
+
+    template <class Func>
+    typename std::enable_if_t<std::is_same<Func, Max>::value, Lattice<bool, Or>>
+    greater_than_or_eq(Lattice<T, Func> target) {
+       auto gt = target.reveal();
+       return Lattice(gt>this->threshold || gt==this->threshold, Or{});
+    }
+
+    template <class Func>
+    typename std::enable_if_t<std::is_same<Func, Min>::value, Lattice<bool, Or>>
+    less_than(Lattice<T, Func> target) {
+       return Lattice(target.reveal()<this->threshold, Or{});
+    }
+
+    template <class Func>
+    typename std::enable_if_t<std::is_same<Func, Min>::value, Lattice<bool, Or>>
+    less_than_or_eq(Lattice<T, Func> target) {
+       auto lt = target.reveal();
+       return Lattice(lt<this->threshold || lt==this->threshold, Or{});
+    }
 };
 
 template <class Func, class T>
